810d3514febc2f9574e845ac9ea108de3c76b977
Shawn Harper <harper.root3@gmail.com>
2021-03-13 14:18:01 -0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
 # Folders to backup
-backup_dir="/home/thor/.config/"
+backup_dir=/home/thor/.config/
 
 # Destination Directories
-dest_dir="/mnt/storage/backups"
+dest_dir=/mnt/storage/backups
 
 # Create archive filename.
 timestamp=$(date +"%Y%m%d")
 hostname=$(hostname -s)
-archive_file="mediaServer-$hostname-$timestamp.tgz"
+archive_file=mediaServer"_"$hostname"_"$timestamp.tgz
 
 # Print start status message.
 echo "Backing up $backup_dir to $dest_dir/$archive_file"
 echo
 
+# Create TMP dir
+mkdir $dest_dir/tmp_$timestamp
+
 # Backup the files using tar.
-rsync -av --exclude={'excludes.txt'} $backup_dir "$dest_dir/tmp"
-tar -czf  $dest_dir/$archive_file $dest_dir/tmp
+rsync -ra --exclude-from="excludes.txt" $backup_dir "$dest_dir/tmp_$timestamp"
+tar -cf  $dest_dir/$archive_file $dest_dir/tmp_$timestamp
+
+# Cleanup and remove old tmp directory
+rm -R $dest_dir/tmp_$timestamp
 
 # Print end status message.
 echo
-echo "Backup complete."
-date
-
-# Long listing of files in $dest to check file sizes.
-ls -lh $dest_dir
+echo "Backup complete. Removed tmp directory."
