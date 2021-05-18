4ca063b4ebb10e87bc4542aa2b3fd25fab0eadbf
charybshawn <charybshawn@gmail.com>
2021-05-18 09:50:29 -0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,6 +2,8 @@
 
 #Bash Script Directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+GREEN='0;32'
+NC='\033[0m' # No Color
 
 # Folders to backup
 backup_dir=/home/thor/.config/
@@ -24,21 +26,23 @@ archive_file=mediaServer"_"$timestamp.tgz
 echo "Backing up the contents of: $backup_dir and archiving to $dest_dir/$archive_file"
 echo
 
-echo "Shutting down all docker containers.."
+printf "Shutting down all docker containers.."
 # Close all docker containers while we complete the backup
 docker kill $(docker ps -q) >/dev/null
-echo "-> DONE"
+printf " ${GREEN}DONE${NC}"
 
 # Create TMP dir
 mkdir -p $temp_dir
 
 # Backup the files using tar.
 echo "Pulling in all the files and prepping archive.."
-rsync --info=progress2 -ra --no-o --no-g --no-perms --exclude-from="$SCRIPT_DIR/excludes.txt" $backup_dir "$dest_dir/tmp_$timestamp"
+rsync --info=progress2 -ra --no-o --no-g --no-perms --exclude-from="$SCRIPT_DIR/excludes.txt" $backup_dir $temp_dir
 echo
 
 echo "Compressing and moving files.."
-tar -czf --totals=SIGUSR1 --verbose $dest_dir/$archive_file $dest_dir/tmp_$timestamp
+#tar -czf --totals=SIGUSR1 --verbose  $dest_dir/tmp_$timestamp
+tar czf - $temp_dir | pv -s $(du -sb $dest_dir/$archive_file | awk '{print $1}') | gzip > $archive_file
+
 
 echo "Restarting all docker containers.."
 # Restart all docker containers
